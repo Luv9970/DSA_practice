c118c7c0f5b410ba6e2fa05e98f564fc8f40c68a
Luv <ir>
2025-09-20 13:06:36 +0530

    fourteenth commit

---
+++ Stacks/Stacks1.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // Using arrays to reverse a stack:
+    // stack<int> st;
+    // st.push(4);
+    // st.push(3);
+    // st.push(2);
+    // st.push(1);
+    // st.push(8);
+
+    // int X = 25;
+    // vector<int> ans(st.size(),0);
+    // int i=0;
+    // while(!st.empty()){
+    //     ans[i] = st.top();
+    //     st.pop();
+    //     i++;
+    // }
+    // st.push(X);
+    // for(int i=ans.size() -1 ; i>=0 ; i--){
+    //     st.push(ans[i]);
+    // }
+
+    // Printing the stack:
+    // while(!st.empty()){
+    //     cout << st.top() << " ";
+    //     st.pop();
+    // }
+
+
+
+
+
+    // Make the array beautiful:
+    vector<int> arr={2,3,5,-4,6,-2,-8,9};
+    
+
+    // string mainupulation:
+    vector<string> v={"ab" , "ac" , "da" , "da" , "ac" , "db" , "ca"};
+
+
+    // Parenthesis is valid or not:
+    string str = "((())())";
+
+
+    // Minimum add to make the parenthesis valid:
+    return 0;
+}
