594722ef1c17c0028c385f45c00ae1a0d3e96e01
Luv <ir>
2025-11-20 11:13:00 +0530

    seventeenth Commit

---
+++ AccentureJapan_questions/questions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    while(1){
+        string s;
+        cin >> s;
+        if(s == "Yes"){
+            cout << "cool" << endl;
+        }else{
+            break;
+        }
+    }
+
+    return 0;
+}
